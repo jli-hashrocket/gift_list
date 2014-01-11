7f89e68544aba829d22071a8a3216cb30ee62f21
macheteshuffle <jeff.li029@gmail.com>
2014-01-11 14:19:48 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GiftList::Application.config.secret_key_base = '1214d86422961472f18c56884ea3f7edae73feec472f13b64c614abddf76acb3a8e9976a14d678bae65e8421b6f44cbe10af5d0d234b94a3ff557c012c2a71e7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GiftList::Application.config.session_store :cookie_store, key: '_gift_list_session'
