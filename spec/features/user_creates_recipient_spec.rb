10fbf8d6f88ccd039522fb8c9bf173da626fbdb2
macheteshuffle <jeff.li029@gmail.com>
2014-02-19 16:36:40 -0500

    created Relationship model

---
+++ spec/features/user_creates_recipient_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+feature 'User creates gift list recipient', %q{
+  User Story:
+  As a User,
+  I want to create a gift list recipient,
+  So that I can keep track of recipients
+} do
+  # Acceptance Criteria:
+  # * I must go to the index
+  # * I must add a new recipient
+  # * I must save the recipient
+
+  scenario 'creates gift list entry' do
+    visit root_path
+    click_on 'New Gift Recipient'
+
+    fill_in 'First Name', with: "Jason"
+    fill_in 'Last Name', with: "Li"
+    select 'Family', from: "Relationship"
+    click_on 'Create Gift Recipient'
+
+    expect(page).to have_content("A new gift recipient has successfully been created")
+  end
+end
