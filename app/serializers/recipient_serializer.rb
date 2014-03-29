b7d7bd3c78bde57f0ff2d4841dd6b065a18ad0f4
macheteshuffle <jeff.li029@gmail.com>
2014-03-28 22:50:12 -0400

    creating has many relationship to gifts for recipient serializer

--- app/serializers/recipient_serializer.rb
+++ app/serializers/recipient_serializer.rb
@@ -2,5 +2,6 @@ class RecipientSerializer < ActiveModel::Serializer
   embed :id
   attributes :id, :first_name, :last_name
   has_one :relationship, key: :relationship
+  has_many :gifts, key: :gifts
 
 end
